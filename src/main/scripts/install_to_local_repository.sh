9bbd3defc5af9158138768ba61066878fd8da7af
Atte Hinkka <atte.hinkka@iki.fi>
2013-06-13 20:08:37 +0300

    Remove extraneous script for install does the same

--- src/main/scripts/install_to_local_repository.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mvn install::install-file \
-    -Dfile=target/bmgraph-1.0-SNAPSHOT.jar \
-    -DgroupId=biomine.bmgraph \
-    -DartifactId=bmgraph \
-    -Dversion=1.0-SNAPSHOT \
-    -Dpackaging=jar
